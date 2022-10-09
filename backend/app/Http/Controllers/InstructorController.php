79ce3898b49ee25be4ece2bf7a8cbd413daf4517
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-09 14:09:13 +0300

    Backend: Added InstructorController

---
+++ backend/app/Http/Controllers/InstructorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\UserType;
+use App\Models\Course;
+use App\Models\Enroll;
+use Auth;
+use Validator;
+
+class InstructorController extends Controller
+{
+    //
+}
